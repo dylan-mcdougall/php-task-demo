d73716917a4c5a7d7d301a3132b782e6e5e8027b
Dylan <dylan.7mcdougall@gmail.com>
2024-02-18 16:10:09 -0800

    initial structure for index file

--- index.php
+++ index.php
@@ -6,6 +6,18 @@
     <title>PHP Task Manager</title>
 </head>
 <body>
-    
+    <div class='page-wrapper'>
+        <div class='page-flex'>
+            <div class='header-wrapper'>
+                <h1>AWSome PHProject</h1>
+            </div>
+            <div class='interface'>
+                <button class='add-task'>Add Task</button>
+            </div>
+            <div class='primary-body'>
+
+            </div>
+        </div>
+    </div>
 </body>
 </html>
